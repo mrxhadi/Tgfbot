0a3aae4ae13814134243c4001d0e6eaeccea2ff5
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-13 00:08:53 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 import os
 from telethon import TelegramClient
+from telethon.sessions import StringSession
 from handlers import send_database, receive_database
 from forwarder import forward_message
 
@@ -7,7 +8,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 STRING_SESSION = os.getenv("STRING_SESSION")
 
-client = TelegramClient("bot", API_ID, API_HASH).start(bot_token=STRING_SESSION)
+client = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 
 client.add_event_handler(send_database)
 client.add_event_handler(receive_database)
@@ -15,4 +16,5 @@ client.add_event_handler(forward_message)
 
 if __name__ == "__main__":
     print("Bot is running...")
+    client.start()
     client.run_until_disconnected()
