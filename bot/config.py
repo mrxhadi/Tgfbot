33c0372c045eb98312d710be57bea4cf2d7baf7e
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-12 23:36:00 +0330

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+STRING_SESSION = os.getenv("STRING_SESSION")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+DATABASE_PATH = "database.json"
