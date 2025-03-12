fa9f12f3c2e24b70807fba0a9de6dfb8dc1b695a
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-12 23:49:32 +0330

    Update filters.py

--- bot/filters.py
+++ bot/filters.py
@@ -1,1 +1,21 @@
+from database import Database
 
+db = Database()
+
+def process_message(text: str) -> str | None:
+    filters = db.get_filters()
+
+    
+    for word in filters["block_words"]:
+        if word in text:
+            return None
+
+    
+    for word in filters["remove_words"]:
+        text = text.replace(word, "")
+
+    
+    for old_word, new_word in filters["replace_words"].items():
+        text = text.replace(old_word, new_word)
+
+    return text.strip() if text.strip() else None
