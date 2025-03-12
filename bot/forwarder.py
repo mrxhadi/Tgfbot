712a02aee6224b07db990774e95783035cd99ff5
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-12 23:50:30 +0330

    Update forwarder.py

--- bot/forwarder.py
+++ bot/forwarder.py
@@ -1,1 +1,19 @@
+from telethon import events
+from database import Database
+from filters import process_message
 
+db = Database()
+
+@events.register(events.NewMessage(chats=list(db.get_channels().keys())))
+async def forward_message(event):
+    source_chat = event.chat_id
+    dest_chat = db.get_channels().get(str(source_chat))
+
+    if not dest_chat:
+        return
+
+    text = event.raw_text
+    processed_text = process_message(text)
+
+    if processed_text:
+        await event.client.send_message(int(dest_chat), processed_text)
