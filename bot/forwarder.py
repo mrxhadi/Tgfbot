9128abea84af3f7785ad49abf58534b39082546a
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-13 00:53:46 +0330

    Update forwarder.py

--- bot/forwarder.py
+++ bot/forwarder.py
@@ -4,15 +4,10 @@ from filters import process_message
 
 db = Database()
 
-@events.register(events.NewMessage(chats=list(map(int, db.get_channels().keys()))))
+@events.register(events.NewMessage(func=lambda e: e.is_channel))
 async def forward_message(event):
     source_chat = event.chat_id
     dest_chat = db.get_channels().get(str(source_chat))
-
-    # جلوگیری از پردازش پیام‌های چت خصوصی
-    if source_chat > 0:  # چت‌های خصوصی آیدی مثبت دارن، ولی کانال‌ها آیدی منفی دارن
-        print(f"[🚫] Ignoring private message from: {source_chat}")
-        return
 
     if not dest_chat:
         print(f"[❌] No destination found for source: {source_chat}")
