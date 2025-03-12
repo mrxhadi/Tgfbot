d80bb774053a5fff2a197ce6c7b713d8323bd63e
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-13 00:56:51 +0330

    Update forwarder.py

--- bot/forwarder.py
+++ bot/forwarder.py
@@ -4,10 +4,15 @@ from filters import process_message
 
 db = Database()
 
-@events.register(events.NewMessage(func=lambda e: e.is_channel))
+@events.register(events.NewMessage(func=lambda e: e.is_channel and str(e.chat_id) in db.get_channels()))
 async def forward_message(event):
     source_chat = event.chat_id
     dest_chat = db.get_channels().get(str(source_chat))
+
+    # نادیده گرفتن پیام‌های فوروارد شده
+    if event.fwd_from:
+        print(f"[🚫] Ignoring forwarded message from: {source_chat}")
+        return
 
     if not dest_chat:
         print(f"[❌] No destination found for source: {source_chat}")
