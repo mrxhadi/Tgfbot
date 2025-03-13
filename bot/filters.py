8d267f72dd792bd5869a54cd4a0ba12595330631
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-13 22:48:59 +0330

    Update filters.py

--- bot/filters.py
+++ bot/filters.py
@@ -1,21 +1,19 @@
 from database import Database
+import re
 
 db = Database()
 
 def process_message(text: str) -> str | None:
     filters = db.get_filters()
 
-    
     for word in filters["block_words"]:
-        if word in text:
+        if re.search(rf'\b{re.escape(word)}\b', text, flags=re.IGNORECASE):
             return None
 
-    
     for word in filters["remove_words"]:
-        text = text.replace(word, "")
+        text = re.sub(rf'\b{re.escape(word)}\b', '', text, flags=re.IGNORECASE)
 
-    
     for old_word, new_word in filters["replace_words"].items():
-        text = text.replace(old_word, new_word)
+        text = re.sub(rf'\b{re.escape(old_word)}\b', new_word, text, flags=re.IGNORECASE)
 
     return text.strip() if text.strip() else None
