0c3a682c9f2949624f7f84b922c5f07f83fe9c63
mrxhadi <60502984+mrxhadi@users.noreply.github.com>
2025-03-13 21:59:55 +0330

    Update database.py

--- bot/database.py
+++ bot/database.py
@@ -24,12 +24,16 @@ class Database:
     def replace_database(self, new_data):
         print(f"[INFO] Replacing database with new data: {new_data}")
         self.data = new_data  
+        print(f"[DEBUG] self.data after assignment: {self.data}")
         self._save_database()  
         self._load_database()  
         print(f"[INFO] Database successfully loaded into memory: {self.data}")
 
     def get_channels(self):
-        return self.data.get("channels", {})
+        print(f"[DEBUG] self.data before returning channels: {self.data}")  # لاگ مقدار کل دیتابیس
+        channels = self.data.get("channels", {})
+        print(f"[DEBUG] get_channels() returning: {channels}")  # لاگ مقدار کانال‌ها
+        return channels
 
     def get_filters(self):
         return self.data.get("filters", {"remove_words": [], "replace_words": {}, "block_words": []})
